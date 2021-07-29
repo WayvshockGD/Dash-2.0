7bbdcd0b7476ac0e5958d5fc22f7ff142f4bc89a
WayvshockGD <pizza5803@gmail.com>
2021-07-29 15:16:47 +0000

    Fixed tags

--- start.js
+++ start.js
@@ -1,5 +1,13 @@
-const Client = require("./lib/Client");
+const Client = require('./lib/Client');
+let config = require('./config.json');
+let mongoose = require('mongoose');
 
 (async () => {
-    await new Client().run();
+	await new Client().run();
+
+	await mongoose.connect(config.dbURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	console.log('Started database...');
 })();
